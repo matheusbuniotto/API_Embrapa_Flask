f67a50c43109f4ba3a78d763e93dfd804b617086
Matheus Buniotto <matheus.buniotto.ds@gmail.com>
2024-10-13 08:05:18 -0300

    ajustes

--- app.py
+++ app.py
@@ -5,6 +5,82 @@ app = Flask(__name__)
 
 BASE_URL = "http://vitibrasil.cnpuv.embrapa.br/download/"
 LOCAL_CSV_DIR = "raw_data"
+
+@app.route('/api/status', methods=['GET'])
+def show_status():
+    return ('The API is running. Check the docummentation at /api/docs \n' 
+            'PT-BR: A API está ativa. Consulte a documentação em /api/docs')
+@app.route('/api/docs', methods=['GET'])
+def api_docs():
+    """
+    Documentação dos endpoints disponíveis na API.
+    """
+    docs = {
+        "status": {
+            "method": "GET",
+            "description": "Verificar se a API está em execução.",
+            "endpoint": "/api/status"
+        },
+        "Comercio": {
+            "method": "GET",
+            "description": ("Busca e processa o arquivo 'Comercio.csv' tanto do sistema local "
+                            "ou da URL remota dependendo do parâmetro 'use_local'."),
+            "endpoint": "/api/Comercio",
+            "query_params": {
+                "use_local": "Opcional, definir como 'true' para buscar o arquivo CSV localmente."
+            }
+
+        },
+        "Produção": {
+            "method": "GET",
+            "description": ("Busca e processa o arquivo 'Producao.csv' tanto do sistema local "
+                            "ou da URL remota dependendo do parâmetro 'use_local'."),
+            "endpoint": "/api/Producao",
+            "query_params": {
+                "use_local": "Opcional, definir como 'true' para buscar o arquivo CSV localmente."
+            }
+        },
+        "Processamento": {
+            "method": "GET",
+            "description": ("Busca e processa arquivos CSV do tipo 'ProcessaFile_type.csv' tanto do "
+                            "sistema local ou da URL remota dependendo do parâmetro 'use_local'."),
+            "endpoint": "/api/processa/file_type",
+            "query_params": {
+                "use_local": "Opcional, definir como 'true' para buscar o arquivo CSV localmente."
+            },
+            "path_params": {
+                "file_type": "Obrigatório, o tipo de arquivo a ser buscado (ex.: 'Frescas', 'Passas', 'Suco', 'Vinhos')."
+            }
+        },
+        "Exportação": {
+            "method": "GET",
+            "description": ("Busca e processa arquivos CSV do tipo 'ExpFile_type.csv' tanto do sistema local "
+                            "ou da URL remota dependendo do parâmetro 'use_local'."),
+            "endpoint": "/api/exp/file_type",
+            "query_params": {
+                "use_local": "Opcional, definir como 'true' para buscar o arquivo CSV localmente."
+            },
+            "path_params": {
+                "file_type": "Obrigatório, o tipo de arquivo a ser consultado (ex.: 'Uva', 'Suco', 'Espumantes', 'VInho')."
+            }
+        },
+        "Importação": {
+            "method": "GET",
+            "description": ("Busca e processa arquivos CSV do tipo 'ImpFile_Type.csv' tanto do sistema local "
+                            "ou da URL remota dependendo do parâmetro 'use_local'."),
+            "endpoint": "/api/imp/file_type",
+            "query_params": {
+                "use_local": "Opcional, definir como 'true' para buscar o arquivo CSV localmente."
+            },
+            "path_params": {
+                "file_type": "Cam,po obrigatório, o tipo de arquivo a ser consultado (ex.: 'Americanas', 'Semclass', 'Viniferas')."
+            }
+        }
+    }
+
+    return jsonify(docs)
+
+
 @app.route('/api/Comercio', methods=['GET'])
 def get_comercio_data():
     url = f"{BASE_URL}Comercio.csv"
@@ -17,6 +93,7 @@ def get_comercio_data():
 
     # Return the processed data as a JSON response
     return jsonify(data)
+
 @app.route('/api/Producao', methods=['GET'])
 def get_producao_data():
     # Check if the 'use_local' query parameter is passed
@@ -40,13 +117,13 @@ def get_producao_data():
     # Return the processed data as a JSON response
     return jsonify(data)
 
-@app.route('/api/ProcessaViniferas', methods=['GET'])
-def get_processa_data():
+@app.route('/api/processa/<file_type>', methods=['GET'])
+def get_processa_files(file_type):
     # Check if the 'use_local' query parameter is passed
     use_local = request.args.get('use_local', 'false').lower() == 'true'
 
-    # Define the CSV file name
-    csv_filename = "ProcessaViniferas.csv"
+    # Define the CSV file name based on the file_type
+    csv_filename = f"Processa{file_type}.csv"
 
     if use_local:
         # Use the CSV file from the local directory
@@ -63,13 +140,36 @@ def get_processa_data():
     # Return the processed data as a JSON response
     return jsonify(data)
 
-@app.route('/api/processa/<file_type>', methods=['GET'])
-def get_processa_files(file_type):
+@app.route('/api/exp/<file_type>', methods=['GET'])
+def get_exportation_files(file_type):
     # Check if the 'use_local' query parameter is passed
     use_local = request.args.get('use_local', 'false').lower() == 'true'
 
     # Define the CSV file name based on the file_type
-    csv_filename = f"Processa{file_type.capitalize()}.csv"
+    csv_filename = f"Exp{file_type}.csv"
+
+    if use_local:
+        # Use the CSV file from the local directory
+        filepath = os.path.join(LOCAL_CSV_DIR, csv_filename)
+        csv_content_tuple = get_csv_content_from_local(filepath)
+    else:
+        # Fetch the CSV file from the URL
+        url = f"{BASE_URL}{csv_filename}"
+        csv_content_tuple = get_csv_content(url)
+
+    # Process the CSV content into JSON
+    data = process_csv_content(csv_content_tuple)
+
+    # Return the processed data as a JSON response
+    return jsonify(data)
+
+@app.route('/api/imp/<file_type>', methods=['GET'])
+def get_importation_files(file_type):
+    # Check if the 'use_local' query parameter is passed
+    use_local = request.args.get('use_local', 'false').lower() == 'true'
+
+    # Define the CSV file name based on the file_type
+    csv_filename = f"Imp{file_type}.csv"
 
     if use_local:
         # Use the CSV file from the local directory
--- processing.py
+++ processing.py
@@ -2,13 +2,13 @@ import csv
 import io
 from collections import OrderedDict
 import requests
+import os
 import unicodedata
 
-
+# Funções da tratamento de strings para padronização
 def remove_special_chars(text):
     normalized = unicodedata.normalize('NFKD', text)
     return ''.join(c for c in normalized if not unicodedata.combining(c))
-
 
 def replace_special_chars(text):
     replacements = {
@@ -18,6 +18,7 @@ def replace_special_chars(text):
     }
     return ''.join(replacements.get(c.lower(), c) for c in text)
 
+# Função que faz um get no csv disponibilizado no site da embrapa
 def get_csv_content(url):
     try:
         response = requests.get(url)
@@ -26,21 +27,27 @@ def get_csv_content(url):
     except requests.RequestException as e:
         return None, f"Error fetching CSV: {str(e)}"
 
-# Function to fetch CSV content from local file
+# Função que consome o csv local na pasta raw_data, caso o request da API seja feito com esse paramêtro
 def get_csv_content_from_local(filepath):
     try:
         with open(filepath, 'r', encoding='utf-8') as file:
-            return file.read(), None
+            return file.read(), filepath, None
     except FileNotFoundError as e:
-        return None, f"Error: File not found - {str(e)}"
+        return None, filepath, f"Error: File not found - {str(e)}"
 
-
-# Function to process CSV and return structured data
-def process_csv_content(csv_content_tuple, delimiter=';'):
-    csv_content, error = csv_content_tuple
+# Função genérica para processamento de todos os CSVs, independente de seu nome ou origem (local ou direto da embrapa)
+def process_csv_content(csv_content_tuple):
+    csv_content, filepath, error = csv_content_tuple
     if error:
-        return {"error": error}  # Handle the error appropriately
+        return {"error": error}
 
+    # Get the file name from the filepath
+    file_name = os.path.basename(filepath) if filepath else ''
+    file_name_without_extension = os.path.splitext(file_name)[0]
+
+    # Define o tipo de delimitador do CSV em casos específicos é necessário ser \t
+
+    delimiter = '\t' if file_name_without_extension in ['ProcessaAmericanas', 'ProcessaMesa', 'ProcessaSemclass'] else ';'
     csv_file = io.StringIO(csv_content)
     csv_reader = csv.reader(csv_file, delimiter=delimiter)
 
@@ -50,15 +57,20 @@ def process_csv_content(csv_content_tuple, delimiter=';'):
     for row in csv_reader:
         formatted_row = OrderedDict()
         formatted_row['id'] = row[0]
-        formatted_row['control'] = replace_special_chars(remove_special_chars(row[1]))
-        formatted_row['product'] = replace_special_chars(remove_special_chars(row[2]))
+
+        # Check if the file name starts with 'Exp' or 'Imp'
+        if file_name_without_extension.startswith(('Exp', 'Imp')):
+            formatted_row['country'] = replace_special_chars(remove_special_chars(row[1]))
+            formatted_row['product'] = file_name_without_extension
+        else:
+            formatted_row['name'] = replace_special_chars(remove_special_chars(row[1]))
+            formatted_row['product'] = replace_special_chars(remove_special_chars(row[2]))
 
         yearly_data = OrderedDict()
         for year, value in zip(headers[3:], row[3:]):
             yearly_data[year] = value
 
-        formatted_row['yearly_data'] = yearly_data
+        formatted_row['year'] = yearly_data
         data.append(formatted_row)
 
     return data
-
